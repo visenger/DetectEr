78d0b4f9c3e0f20019311df13e438ff7424b5fb9
visenger <visenger@gmail.com>
2017-09-15 16:41:29 +0200

    mutual information between columns;

--- src/main/scala/de/playground/FeaturesAnalysisPlayground.scala
+++ src/main/scala/de/playground/FeaturesAnalysisPlayground.scala
@@ -1,12 +1,11 @@
 package de.playground
 
-import com.typesafe.config.ConfigFactory
 import de.evaluation.f1.{Eval, FullResult}
 import de.evaluation.util.{DataSetCreator, SparkLOAN}
 import de.experiments.ExperimentsCommonConfig
 import de.experiments.features.generation.FeaturesGenerator
 import de.experiments.models.combinator.{Bagging, Stacking}
-import de.model.util.Features
+import de.model.util.{Features, NumbersUtil}
 import org.apache.spark.SparkContext
 import org.apache.spark.ml.feature.ChiSqSelector
 import org.apache.spark.ml.linalg
@@ -17,6 +16,18 @@ import org.apache.spark.sql.DataFrame
 
 class FeaturesAnalysisPlayground {
 
+}
+
+case class CountsTableRow(x: Double, y: Double, total: Double, xyCount: Double, xCount: Double, yCount: Double)
+
+case class MutualInformation(column1: String, column2: String, mi: Double) {
+  override def toString = {
+    s"Mutual information between $column1 and $column2 = $mi"
+  }
+}
+
+case class Correlation(column1: String, column2: String, value: Double) {
+  override def toString = s"Correlation between $column1 and $column2 is: ${NumbersUtil.round(value, 4)}"
 }
 
 object FeaturesAnalyzerPlayground extends ExperimentsCommonConfig {
@@ -97,12 +108,15 @@ object FeaturesAnalyzerPlayground extends ExperimentsCommonConfig {
   }
 
   def main(args: Array[String]): Unit = {
-    val experimentsConf = ConfigFactory.load("experiments.conf")
     val datasets = Seq("blackoak", "hosp", "salaries", "flights")
-    //val datasets = Seq("hosp")
+    //val datasets = Seq("blackoak")
     datasets.foreach(dataset => {
 
-      println(s"running on ${dataset.toUpperCase()} ")
+      println(
+        s"""
+           |
+           |running on ${dataset.toUpperCase()}
+           |""".stripMargin)
       computePredictivityOfFeatures(dataset)
     })
 
@@ -217,63 +231,94 @@ object FeaturesAnalyzerPlayground extends ExperimentsCommonConfig {
         //Set of content-based metadata, such as "attrName", "attrType", "isNull", "missingValue", "attrTypeIndex", "attrTypeVector", "isTop10"
         val contentBasedFeaturesDF: DataFrame = generator.plgrd_generateContentBasedMetadata(session, dirtyDF)
 
-
         //general info about fd-awarnes of each cell
         val generalInfoDF = generator.plgrd_generateGeneralInfoMetadata(session, dirtyDF, generator.allFDs)
 
         val allMetadata: DataFrame = contentBasedFeaturesDF
-          .join(generalInfoDF, Seq(FullResult.recid, FullResult.attrnr, "value", "attrName"))
-
-        allMetadata.printSchema()
+          .join(generalInfoDF, Seq(FullResult.recid, FullResult.attrnr))
 
         val trainDataPath = allTrainData.getOrElse(dataset, "unknown")
-        val testDataPath = allTestData.getOrElse(dataset, "unknown")
 
         val trainSystemsAndLabel: DataFrame = DataSetCreator.createFrame(session, trainDataPath, FullResult.schema: _*)
-        trainSystemsAndLabel.printSchema()
 
         var systemsAndMetaDF = trainSystemsAndLabel.join(allMetadata, Seq(FullResult.recid, FullResult.attrnr))
-        systemsAndMetaDF.printSchema()
 
-        val labels: RDD[Double] = systemsAndMetaDF.select("label").rdd.map(row => row.getString(0).toDouble)
+        //todo: convert all str columns into double
 
-        generator.allFDs.foreach(fd => {
-          val fdPresence: RDD[Double] = systemsAndMetaDF.select(s"${fd.toString}").rdd.map(row => row.getDouble(0))
-          val correlation: Double = Statistics.corr(labels, fdPresence, "pearson")
-          println(s"Correlation between label and the fd: ${fd.toString} is: $correlation")
-        })
-
-
-        Seq("missingValue", "isTop10").foreach(metadata => {
-          val metaInfo: RDD[Double] = systemsAndMetaDF.select(metadata).rdd.map(row => row.getDouble(0))
-          val correlation: Double = Statistics.corr(labels, metaInfo, "pearson")
-          println(s"Correlation between label and the column: $metadata is: $correlation")
-        })
-
-        //todo: we have to convert systems columns from string to double
         import org.apache.spark.sql.functions._
-
-        val convert_str_to_double = udf {
+        val convert_to_double = udf {
           value: String => value.toDouble
         }
 
-        FullResult.tools.foreach(tool => {
+        systemsAndMetaDF = systemsAndMetaDF
+          .withColumn(s"${FullResult.label}-tmp", convert_to_double(systemsAndMetaDF(FullResult.label)))
+          .drop(FullResult.label)
+          .withColumnRenamed(s"${FullResult.label}-tmp", FullResult.label)
 
-          val metaInfo: RDD[Double] = systemsAndMetaDF.select(tool).rdd.map(row => row.getString(0).toDouble)
-          val correlation: Double = Statistics.corr(labels, metaInfo, "pearson")
-          println(s"Correlation between label and the system column: ${getName(tool)} is: $correlation")
+        FullResult.tools.foreach(tool => {
+          systemsAndMetaDF = systemsAndMetaDF
+            .withColumn(s"$tool-tmp", convert_to_double(systemsAndMetaDF(tool)))
+            .drop(tool)
+            .withColumnRenamed(s"$tool-tmp", tool)
+        })
+
+        systemsAndMetaDF = systemsAndMetaDF.withColumn("allZeros", lit(0.0))
+
+        val labelAndMIssing: RDD[(Double, Double)] = systemsAndMetaDF
+          .select(FullResult.label, "missingValue")
+          .rdd.map(row => {
+          (row.getDouble(0), row.getDouble(1))
+        })
+        labelAndMIssing.countByValue().foreach(item => println(s" (${item._1}) count ${item._2}"))
+
+        val labels: RDD[Double] = systemsAndMetaDF
+          .select(FullResult.label)
+          .rdd
+          .map(row => row.getDouble(0))
+
+        val coef = "pearson"
+        //val coef = "spearman"
+        val correlationsWithFDs: Seq[Correlation] = generator.allFDs.map(fd => {
+          val fdPresence: RDD[Double] = systemsAndMetaDF.select(s"${fd.toString}").rdd.map(row => row.getDouble(0))
+          val correlation: Double = Statistics.corr(labels, fdPresence, coef)
+          Correlation(FullResult.label, fd.toString, NumbersUtil.round(correlation, 4))
 
         })
 
+        val metadataColumns = Seq("missingValue", "isTop10", "allZeros")
+        val metadataCorrelations = metadataColumns.map(metadata => {
+          val metaInfo: RDD[Double] = systemsAndMetaDF.select(metadata).rdd.map(row => row.getDouble(0))
+          val correlation: Double = Statistics.corr(labels, metaInfo, coef)
+          Correlation(FullResult.label, metadata, NumbersUtil.round(correlation, 4))
+        })
 
-        //Run combinations.
-        //        val stacking = new Stacking()
-        //        val evalStacking: Eval = stacking.performStackingOnToolsAndMetadata(session, trainChiSq, testChiSq)
-        //        evalStacking.printResult(s"STACKING on $dataset")
-        //
-        //        val bagging = new Bagging()
-        //        val evalBagging: Eval = bagging.performBaggingOnToolsAndMetadata(session, trainChiSq, testChiSq)
-        //        evalBagging.printResult(s"BAGGING on $dataset")
+        val correlationsWithTools: Seq[Correlation] = FullResult.tools.map(tool => {
+          val metaInfo: RDD[Double] = systemsAndMetaDF.select(tool).rdd.map(row => row.getDouble(0))
+          val correlation: Double = Statistics.corr(labels, metaInfo, coef)
+          Correlation(FullResult.label, tool, NumbersUtil.round(correlation, 4))
+
+        })
+        println("CORRELATIONS")
+
+        Seq(correlationsWithFDs ++ metadataCorrelations ++ correlationsWithTools)
+          .flatten
+          .foreach(cor => println(cor.toString))
+
+        val fds: List[String] = generator.allFDs.map(_.toString)
+        val allTools = FullResult.tools
+
+
+        println("MUTUAL INFORMATION")
+
+        val fdsMIs: Seq[MutualInformation] = computeMutualInformation(systemsAndMetaDF, fds)
+
+        val metadataMIs: Seq[MutualInformation] = computeMutualInformation(systemsAndMetaDF, metadataColumns)
+
+        val toolsMIs: Seq[MutualInformation] = computeMutualInformation(systemsAndMetaDF, allTools)
+
+        Seq(fdsMIs ++ metadataMIs ++ toolsMIs)
+          .flatten
+          .foreach(mi => println(mi.toString))
 
       }
     }
@@ -281,5 +326,33 @@ object FeaturesAnalyzerPlayground extends ExperimentsCommonConfig {
 
   }
 
+  /** Mutual Information: https://nlp.stanford.edu/IR-book/html/htmledition/mutual-information-1.html */
+  def computeMutualInformation(systemsAndMetaDF: DataFrame, columns: Seq[String]): Seq[MutualInformation] = {
+    //todo 1: select the domain values of each column
+    val xs, ys = Seq(1.0, 0.0)
 
+    val total: Double = systemsAndMetaDF.select(FullResult.label).count().toDouble
+    val mutualInformations: Seq[MutualInformation] = columns.map(col => {
+      val column = col.toString
+
+      val countsTable: Seq[CountsTableRow] = for {x <- xs; y <- ys} yield {
+
+        val xyCount = systemsAndMetaDF.select(FullResult.label, column)
+          .where(systemsAndMetaDF(FullResult.label) === x && systemsAndMetaDF(column) === y).count().toDouble
+        val xCount = systemsAndMetaDF.select(FullResult.label, column)
+          .where(systemsAndMetaDF(FullResult.label) === x).count().toDouble
+        val yCount = systemsAndMetaDF.select(FullResult.label, column)
+          .where(systemsAndMetaDF(column) === y).count().toDouble
+
+        CountsTableRow(x, y, total, xyCount, xCount, yCount)
+      }
+      /** Mutual Information formula: https://nlp.stanford.edu/IR-book/html/htmledition/mutual-information-1.html */
+      val mutualInformation: Double = countsTable.foldLeft(0.0)((acc, row) => {
+        acc + (row.xyCount / row.total) * Math.log((row.total * row.xyCount) / (row.xCount * row.yCount))
+      })
+      MutualInformation(FullResult.label, column, NumbersUtil.round(mutualInformation, 4))
+    })
+    mutualInformations
+
+  }
 }
