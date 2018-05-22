dfbd7f0b753cd25ced79b6cf95f19bdd252be0c1
visenger <visenger@gmail.com>
2018-05-22 20:00:20 +0200

    dataset preparation: flattening and estimating diff;2

--- src/main/scala/de/playground/MetadataBasedErrorDetectorPlaygroud.scala
+++ src/main/scala/de/playground/MetadataBasedErrorDetectorPlaygroud.scala
@@ -2,37 +2,39 @@ package de.playground
 
 import de.evaluation.data.metadata.MetadataCreator
 import de.evaluation.util.SparkLOAN
+import de.experiments.ExperimentsCommonConfig
+import de.util.DatasetFlattener
 import org.apache.spark.sql.DataFrame
 
 /**
   * prototype to an error detection based on metadata information;
   */
-class MetadataBasedErrorDetectorPlaygroud {
+object MetadataBasedErrorDetectorPlaygroud extends ExperimentsCommonConfig {
 
-  val metadataPath = "/Users/visenger/research/datasets/craft-beers/craft-cans/metadata/dirty-2-SCDP-1.1-SNAPSHOT.jar2018-05-22T150721_stats"
 
   def main(args: Array[String]): Unit = {
     SparkLOAN.withSparkSession("metadata reader") {
       session => {
-        val creator = MetadataCreator()
 
-        val fullMetadataDF: DataFrame = creator.getFullMetadata(session, metadataPath)
-        fullMetadataDF.printSchema()
+        Seq("flights", "beers").foreach(dataset => {
+          println(s"processing $dataset.....")
+          val metadataPath = allMetadataByName.getOrElse(dataset, "unknown")
+          val creator = MetadataCreator()
 
-        /**
-          * root
-          * |-- nulls count: long (nullable = true)
-          * |-- % of nulls: long (nullable = true)
-          * |-- % of distinct vals: long (nullable = true)
-          * |-- top10: array (nullable = true)
-          * |    |-- element: string (containsNull = true)
-          * |-- freqTop10: array (nullable = true)
-          * |    |-- element: long (containsNull = true)
-          * |-- histogram: array (nullable = true)
-          * |    |-- element: string (containsNull = true)
-          * |-- attrName: string (nullable = true)
-          */
-        fullMetadataDF.show(false)
+          val fullMetadataDF: DataFrame = creator.getFullMetadata(session, metadataPath)
+          //          fullMetadataDF.printSchema()
+
+          fullMetadataDF.show(false)
+
+          val dirtyDF: DataFrame = DatasetFlattener().onDataset(dataset).flattenDirtyData(session)
+          dirtyDF.show()
+          DatasetFlattener().onDataset(dataset).flattenCleanData(session).show()
+          DatasetFlattener().onDataset(dataset).makeFlattenedDiff(session).show()
+
+          dirtyDF.join(fullMetadataDF, Seq("attrName")).show()
+
+
+        })
 
 
       }
